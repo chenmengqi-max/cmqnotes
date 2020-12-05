112035f3e0444c4dd64ea01bb278861e5037c5af
chenmengqi-max <73648976+chenmengqi-max@users.noreply.github.com>
2020-12-05 18:55:06 +0800

    new

---
+++ java learn/test1/src/helloworld.java
@@ -0,0 +1,22 @@
+/**
+ * @author： 上海大学-15122657陈孟琦
+ * @date： 2020/12/2 - 17:06
+ */
+
+public class helloworld {
+    public static void main(String[] args) {
+        int q = 2, w = 4;
+        sum(4, 7 );
+        System.out.println(sum(q,w));
+        System.out.println(q);
+        System.out.println(w);
+
+    }
+
+
+    public static int sum(int a, int b) {
+        int c;
+        c = a + b;
+        return c;
+    }
+}
